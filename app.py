99873c0b07fa91f681fea5d6dbf46e7532938e45
MWIHAKI NGIGI <ngigileah92@gmail.com>
2024-08-25 15:22:17 +0300

    updates

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ class ImageToPDFConverter:
         self.output_pdf_name = tk.StringVar()
         self.selected_images_listbox = tk.Listbox(root, selectmode=tk.MULTIPLE)
 
-        self.initialize.ui()
+        self.initialize_ui()
 
     def initialize_ui(self):
         title_label = tk.Label(self.root, text="Image to PDF Converter", font=("Helvetica", 16, "bold"))
@@ -18,6 +18,7 @@ class ImageToPDFConverter:
 def main():
     root = tk.Tk()
     root.title("Image to PDF")
+    converter = ImageToPDFConverter(root)
     root.geometry("400x600")
     root.mainloop()
 
