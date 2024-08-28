1b9ab8e4730c1cac18b03d5e5443b5c3ce3e50aa
MWIHAKI NGIGI <ngigileah92@gmail.com>
2024-08-28 11:30:39 +0300

    Updates

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 from tkinter import filedialog
+from reportlab.pdfgen import canvas
+from PIL import Image
 import os
 
 class ImageToPDFConverter:
@@ -30,9 +32,43 @@ class ImageToPDFConverter:
         convert_button.pack(pady=(20, 40))
 
     def select_images(self):
-        self.image_paths = filedialog.askopenfilenames(title="Select Images", )
+        self.image_paths = filedialog.askopenfilenames(title="Select Images", filetypes=[("Image files", "*.png;*.jpg;*.jpeg")])
+        self.update_selected_images_listbox()
 
+    def update_selected_images_listbox(self):
+        self.selected_images_listbox.delete(0, tk.END)
+
+        for image_path in self.image_paths:
+            _, image_path = os.path.split(image_path)
+            self.selected_images_listbox.insert(tk.END, image_path)
+
+    def convert_images_to_pdf(self):
+        if not self.image_paths:
+            return
         
+        output_pdf_path = self.output_pdf_name.get() + ".pdf" if self.output_pdf_name.get() else "output.pdf"
+
+        pdf = canvas.Canvas(output_pdf_path, pagesize=(612, 792))
+
+        for image_path in self.image_paths:
+            img = Image.open(image_path)
+            available_width = 540
+            available_height = 720
+            scale_factor = min(available_width / img.width, available_height / img.height)
+            new_width = img.width * scale_factor
+            new_height = img.height * scale_factor
+            x_centered = (612 - new_width) / 2
+            y_centered = (782 - new_height) / 2
+
+            pdf.setFillColorRGB(255, 255, 255)
+            pdf.rect(0, 0, 612, 792, fill=True)
+            pdf.drawInlineImage(img, x_centered, y_centered, width=new_width, height=new_height)
+            pdf.showPage()
+
+        pdf.save()
+
+
+
 def main():
     root = tk.Tk()
     root.title("Image to PDF")
